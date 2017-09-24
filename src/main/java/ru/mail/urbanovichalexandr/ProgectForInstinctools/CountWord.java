98af2655da059e5efe8dc5bdc1809ad758d02259
Александр <Urbanovichalexandr@mail.ru>
2017-09-24 17:48:19 +0200

    no message

--- src/main/java/ru/mail/urbanovichalexandr/ProgectForInstinctools/CountWord.java
+++ src/main/java/ru/mail/urbanovichalexandr/ProgectForInstinctools/CountWord.java
@@ -1,8 +1,18 @@
 package ru.mail.urbanovichalexandr.ProgectForInstinctools;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class CountWord {
 
@@ -20,9 +30,43 @@ public class CountWord {
 			}
 		}
 
-//сортируем коллекцию по количеству повторений слов в тексте
-		counterMap.entrySet().stream().sorted((Map.Entry.<String, Integer>comparingByValue()).reversed()).forEach(System.out::println);
 
+//counterMap.entrySet().stream().sorted((Entry.<String, Integer>comparingByValue()).reversed()).forEach(System.out::println);
+	
+//сортируем коллекцию по количеству повторений слов в тексте		
+		Map<String, Integer> sortedMap = sortByValue(counterMap);
+		printMap(sortedMap);
+		
 		return null;
 	}
+	
+	private static Map<String, Integer> sortByValue(Map<String, Integer> counterMap){
+		List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(counterMap.entrySet());
+		Collections.sort(list, new Comparator<Map.Entry<String, Integer>>(){
+
+			@Override
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+				return (o2.getValue()).compareTo(o1.getValue());
+			}
+		});
+		
+		Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
+		for(Map.Entry<String, Integer> entry : list) {
+			sortedMap.put(entry.getKey(), entry.getValue());
+		}
+		
+		return sortedMap;
+	}
+	
+	public static <K, V> void printMap(Map<K, V> map) {
+		int i = 0;
+		for(Map.Entry<K, V> entry : map.entrySet()) {
+			if(i < 10) {
+				System.out.println(entry.getKey() + "=" + entry.getValue());
+				i++;
+			}
+		}
+	}
+
 }
+
